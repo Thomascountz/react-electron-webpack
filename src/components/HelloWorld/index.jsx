5734572991b26f56da9db0f860117c73949e5378
Thomas Countz <thomascountz@gmail.com>
2018-08-17 11:32:58 -0400

    add jest/enzyme

--- src/components/HelloWorld/index.jsx
+++ src/components/HelloWorld/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { hot } from "react-hot-loader";
 import "./styles.css";
 
@@ -9,20 +9,17 @@ import webPackLogo from "../../assets/webpack.png";
 const logos = [reactLogo, electronLogo, webPackLogo];
 
 import Logo from "../Logo/index";
+const renderLogos = logos.map((logo, index) => {
+  return <Logo src={logo} key={index} />;
+});
 
-class HelloWorld extends Component {
-  render() {
-    const renderLogos = logos.map((logo, index) => {
-      return <Logo src={logo} key={index} />;
-    });
-
-    return (
-      <div className="helloWorld">
-        <h1> Hello, World! </h1>
-        {renderLogos}
-      </div>
-    );
-  }
-}
+const HelloWorld = () => {
+  return (
+    <div className="helloWorld">
+      <h1> Hello, World! </h1>
+      {renderLogos}
+    </div>
+  );
+};
 
 export default hot(module)(HelloWorld);
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./styles.css";
 
 const Logo = props => {
   return <img src={props.src} className="Logo" />;
+};
+
+Logo.propTypes = {
+  src: PropTypes.string.isRequired
 };
 
 export default Logo;
